7b4181c9700e54eaba95913397357274f7361a35
Pocket31 <cr796@yandex.ru>
2023-10-11 18:20:03 +0300

    add button

--- main.py
+++ main.py
@@ -4,38 +4,27 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 
+
 from TOKEN import TELEGRAM_TOKEN
 
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
-# urlkb = InlineKeyboardMarkup(row_width=1)
-# urlButton = InlineKeyboardButton(
-#     text='Перейти в блог Skillbox', url='https://skillbox.ru/media/code/')
-# urlButton2 = InlineKeyboardButton(
-#     text='Перейти к курсам Skillbox', url='https://skillbox.ru/code/')
-# urlkb.add(urlButton, urlButton2)
-
-
-@dp.message_handler(commands='ссылки')
-async def url_command(message: types.Message):
-    await message.answer('Полезные ссылки:', reply_markup=urlkb)
-
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    kb = [
-        [
-            KeyboardButton(text="Товары"),
-            KeyboardButton(text="Баланс?"),
-            KeyboardButton(text="FAQ"),
-            KeyboardButton(text="Кабинет"),
-            KeyboardButton(text="Поддержка"),
-        ],
-    ]
-    keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
 
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row(
+        KeyboardButton(text="Товары"),
+        KeyboardButton(text="Баланс"),
+        KeyboardButton(text="FAQ"),
+    )
+    keyboard.row(
+        KeyboardButton(text="Кабинет"),
+        KeyboardButton(text="Поддержка"),
+    )
     await message.reply("Привет!\nЯ Эхобот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.", reply_markup=keyboard)
 
 
@@ -43,10 +32,20 @@ async def send_welcome(message: types.Message):
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
+inline_kb = InlineKeyboardMarkup(row_width=2)
+email_highButton = InlineKeyboardButton(
+    text='Почты HIGH репутация', callback_data='lol')
+inline_kb.add(email_highButton)
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+
+@dp.message_handler(text='Товары')
+async def category_items(message: types.Message):
+
+    await message.reply("Выберите категорию:", reply_markup=inline_kb)
+
+# @dp.message_handler()
+# async def echo_message(msg: types.Message):
+#     await bot.send_message(msg.from_user.id, msg.text)
 
 
 if __name__ == '__main__':
